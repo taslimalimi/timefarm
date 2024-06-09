0ff94b551d4aaa80f3c96b61013f9f5bc112ee9e
Ade Arman Wijaya <adearmanw@gmail.com>
2024-06-10 06:59:29 +0700

    first upload

--- timefarm.py
+++ timefarm.py
@@ -110,8 +110,8 @@ def print_welcome_message():
 █▀▀ █░█ ▄▀█ █░░ █ █▄▄ █ █▀▀
 █▄█ █▀█ █▀█ █▄▄ █ █▄█ █ ██▄
           """)
-    print(Fore.GREEN + Style.BRIGHT + "Spell BOT")
-    print(Fore.CYAN + Style.BRIGHT + "Update Link: https://github.com/adearman/spell")
+    print(Fore.GREEN + Style.BRIGHT + "TimeFarm BOT")
+    print(Fore.CYAN + Style.BRIGHT + "Update Link: https://github.com/adearman/timefarm")
     print(Fore.YELLOW + Style.BRIGHT + "Free Konsultasi Join Telegram Channel: https://t.me/ghalibie")
     print(Fore.BLUE + Style.BRIGHT + "Buy me a coffee :) 0823 2367 3487 GOPAY / DANA")
     print(Fore.RED + Style.BRIGHT + "NOT FOR SALE ! Ngotak dikit bang. Ngoding susah2 kau tinggal rename :)")
